0cf9fd000f451577b6090b61af27bf2e1f67a2d9
Caitlin Hinshaw <caitlin.hinshaw@gmail.com>
2020-03-12 01:27:38 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,8 @@
 def oxford_comma(array)
-  if condition
-
-  elsif condition
-
+  if array.length == 1
+    array.join
+  elsif array.length == 2
+    array.join(" and ")
   else
     last_item = array.pop
     list = array.join(", ")
